2ff2214887d1ac92fed931e4f032dae0ecba8b4a
ChenBlue <bob840806@gmail.com>
2017-11-20 22:17:51 +0800

    Fix bugs for the parameter in besselj & add M=16

--- Filtered_Gaussian_autocorrelation.m
+++ Filtered_Gaussian_autocorrelation.m
@@ -1,55 +1,62 @@
-fmT = [0.01 0.1 0.5];
+% Implement a Rayleigh fading channel simulator based on the Filtered 
+% Gaussian Noise method, and plot the channel output autocorrelation
+% for fmT = 0.01, 0.1 and 0.5. 
+fmT = [0.01 0.1 0.5]; % 3 different fm*T
 [row, num] = size(fmT);
-Omgp = 1;
-sample_num = 30000;
-T = 1;
+Omgp = 1; % Set average power as 1
+sample_num = 30000;  % channel output data point
+T = 1;  % simulation step size
 
+% the coefficient of first order low-pass filter.
 sigma = 2-cos(pi.*fmT./2)-sqrt((2-cos(pi.*fmT./2)).^2-1);
-var = (1+sigma)./(1-sigma).*Omgp./2;
+var = (1+sigma)./(1-sigma).*Omgp./2; % variance of Gaussian noise source
 
-w1 = zeros(num, sample_num);
-w2 = zeros(num, sample_num);
+w1 = zeros(num, sample_num); % Gaussian noise source 1 for Gi
+w2 = zeros(num, sample_num); % Gaussian noise source 2 for Gq
 for i = 1:num
     w1(i,:) = normrnd(0,sqrt(var(i)),1,sample_num);
     w2(i,:) = normrnd(0,sqrt(var(i)),1,sample_num);
 end
 
-gI = ones(num, sample_num);
-gQ = ones(num, sample_num);
-
-for i = 1:num
-    for j = 1:sample_num-1
-        gI(i,j+1) = sigma(i)*gI(i,j)+(1-sigma(i))*w1(i,j);
-        gQ(i,j+1) = sigma(i)*gQ(i,j)+(1-sigma(i))*w2(i,j);
-    end
+gI = ones(num, sample_num); % In-phase part of output
+gQ = ones(num, sample_num); % Quadrature part of output
+% Derive the value by low-pass filter
+sigma = sigma';
+for j = 1:sample_num-1
+    gI(:,j+1) = sigma.*gI(:,j)+(1-sigma).*w1(:,j);
+    gQ(:,j+1) = sigma.*gQ(:,j)+(1-sigma).*w2(:,j);
 end
 
 fm = fmT./ T;
-tau = 10./fm;
+tau = 10./fm; % largest tau, Requirement: fm*tau=0~10
+% Autocorrelation for three different fm*T, the size of autocorrelation is
+% different.
 phi1 = zeros(1, tau(1)+1);
 phi2 = zeros(1, tau(2)+1);
 phi3 = zeros(1, tau(3)+1);
 
-gI_shift = zeros(1,sample_num);
-
+% fm*T = 0.01
 for i=0:tau(1)
-    gI_shift = zeros(1,sample_num);
-    gI_shift(1, i+1:end) = gI(1,1:end-i);
-    phi1(1,i+1) = mean(gI(1,:).*gI_shift);
+    gI_shift = zeros(1,sample_num); 
+    gI_shift(1, i+1:end) = gI(1,1:end-i); % gI shift right i index and put it in gI_shift
+    phi1(1,i+1) = mean(gI(1,:).*gI_shift); % Multiply each other and average them
 end
 
+% fm*T = 0.1
 for i=0:tau(2)
     gI_shift = zeros(1,sample_num);
     gI_shift(1, i+1:end) = gI(2,1:end-i);
     phi2(1,i+1) = mean(gI(2,:).*gI_shift);
 end
 
+% fm*T = 0.5
 for i=0:tau(3)
     gI_shift = zeros(1,sample_num);
     gI_shift(1, i+1:end) = gI(3,1:end-i);
     phi3(1,i+1) = mean(gI(3,:).*gI_shift);
 end
 
+% Plot the channel autocorrelation with 3 different fm*T
 figure,plot(fm(1).*(0:tau(1)), phi1./abs(phi1(1)),'r',fm(2).*(0:tau(2)), phi2./abs(phi2(1)),'g',fm(3).*(0:tau(3)), phi3./abs(phi3(1)),'b');
 title('Autocorrelation of Filtered Gaussian Method');
 xlabel('f_m\tau');
